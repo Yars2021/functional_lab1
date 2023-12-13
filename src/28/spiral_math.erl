d42de08dee469e74e72e6146e1a115540d2e5f5e
Yars2021 <79992244+Yars2021@users.noreply.github.com>
2023-12-13 16:34:36 +0300

    Update spiral_math.erl

--- src/28/spiral_math.erl
+++ src/28/spiral_math.erl
@@ -6,4 +6,7 @@ item_s(S1, D1, DD, N) -> S1 + (2 * D1 + DD * (N - 2)) * (N - 1) / 2.
 
 sum_s(S1, D1, DD, N) -> (S1 + (D1 + DD * (N - 2) / 3) * (N - 1) / 2) * N.
 
-spiral_sum(N) -> sum_s(1, 2, 8, N div 2 + 1) + sum_s(1, 4, 8, N div 2 + 1) + sum_s(1, 6, 8, N div 2 + 1) + sum_s(1, 8, 8, N div 2 + 1) - 3.
+spiral_sum(N) -> sum_s(1, 2, 8, N div 2 + 1) +
+                   sum_s(1, 4, 8, N div 2 + 1) +
+                   sum_s(1, 6, 8, N div 2 + 1) +
+                   sum_s(1, 8, 8, N div 2 + 1) - 3.
