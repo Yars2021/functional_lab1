79c4f76e059eb84f559649b019abde9de10aac72
Yars2021 <79992244+Yars2021@users.noreply.github.com>
2023-12-13 16:24:20 +0300

    Update and rename lab1_28_tests.erl to spiral_tests.erl

---
+++ test/28/spiral_tests.erl
@@ -0,0 +1,17 @@
+-module(lab1_28_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+% Reduce with normal recursion
+spiral_sum_test() ->
+    ?assertEqual(spiral_alg:spiral_sum(5), 101),
+    ?assertEqual(spiral_alg:spiral_sum(1001), 669171001).
+
+% Reduce with tail recursion
+spiral_sum_t_test() ->
+    ?assertEqual(spiral_alg:spiral_sum_t(5), 101),
+    ?assertEqual(spiral_alg:spiral_sum_t(1001), 669171001).
+
+% Fold
+spiral_sum_f_test() ->
+    ?assertEqual(spiral_alg:spiral_sum_f(5), 101),
+    ?assertEqual(spiral_alg:spiral_sum_f(1001), 669171001).
