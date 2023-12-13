3fb7a0e01df229fd39b55549f549420ceeafabb0
Yars2021 <79992244+Yars2021@users.noreply.github.com>
2023-12-13 16:26:27 +0300

    Update and rename lab1_28m.erl to spiral_math.erl

--- src/28/spiral_math.erl
+++ src/28/spiral_math.erl
@@ -1,4 +1,4 @@
--module(lab1_28m).
+-module(a).
 
 -export([item_s/4, sum_s/4, spiral_sum/1]).
 
@@ -6,5 +6,8 @@ item_s(S1, D1, DD, N) -> S1 + (2 * D1 + DD * (N - 2)) * (N - 1) / 2.
 
 sum_s(S1, D1, DD, N) -> (S1 + (D1 + DD * (N - 2) / 3) * (N - 1) / 2) * N.
 
-spiral_sum(N) -> 
-	sum_s(1, 2, 8, N div 2 + 1) + sum_s(1, 4, 8, N div 2 + 1) + sum_s(1, 6, 8, N div 2 + 1) + sum_s(1, 8, 8, N div 2 + 1) - 3.
+spiral_sum(N) ->
+	sum_s(1, 2, 8, N div 2 + 1) +
+	sum_s(1, 4, 8, N div 2 + 1) +
+	sum_s(1, 6, 8, N div 2 + 1) +
+	sum_s(1, 8, 8, N div 2 + 1) - 3.
